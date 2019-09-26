bb2834b740a78d99b54ff70a049aae9cc234c1cd
Lilliam Montoya <lilmonrod@gmail.com>
2019-09-25 19:06:12 -0600

    aligning floating-buttons using diferents media queries

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,6 @@
-(function () {
+(function menu() {
   const btnMenu = document.getElementById('btn-menu');
+  const subMenu = document.getElementById('js-sub-menu');
 
   function toggleMenu() {
     const body = document.getElementsByTagName('body')[0];
@@ -7,5 +8,12 @@
     body.classList.toggle('menu-open');
   }
 
+  function toggleSubMenu() {
+    const container = document.getElementById('cont-sub-menu')[0];
+
+    container.classList.toggle('menu-open');
+  }
+
   btnMenu.addEventListener('click', toggleMenu);
-})();
+  subMenu.addEventListener('click', toggleSubMenu);
+}());

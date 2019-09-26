c724bc1e95c8ee6d47d956948167a63e36d3ff31
Lilliam Montoya <lilmonrod@gmail.com>
2019-09-26 14:45:30 -0600

    fixed submenu in mobile and deleted submenu function in menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,5 @@
 (function menu() {
   const btnMenu = document.getElementById('btn-menu');
-  const subMenu = document.getElementById('cont-sub-menu');
 
   function toggleMenu() {
     const body = document.getElementsByTagName('body')[0];
@@ -8,12 +7,5 @@
     body.classList.toggle('menu-open');
   }
 
-  function toggleSubMenu() {
-    const container = document.getElementById('cont-sub-menu');
-
-    container.classList.toggle('menu-open');
-  }
-
   btnMenu.addEventListener('click', toggleMenu);
-  subMenu.addEventListener('click', toggleSubMenu);
 }());
